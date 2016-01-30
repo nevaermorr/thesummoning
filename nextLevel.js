8dc53eb206eefbf05c6c180a4f04d65d4eac2e8b
jklimek <jklimek@fidolabs.com>
2016-01-30 10:02:30 +0100

    Repairing main animation

---
+++ nextLevel.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+import UnityEngine.SceneManagement;
+
+function Awake () {
+	Invoke ("NextLevel", 6);
+}
+
+function Update () {
+	if(Input.anyKey) {
+		NextLevel();
+	}
+}
+
+function NextLevel() {
+	SceneManager.LoadScene("main");
+}
