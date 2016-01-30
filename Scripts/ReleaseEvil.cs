668f2f0d9f28f771119ebad026a0faf881ea9ca2
nevaermorr <pi.wiacek@gmail.com>
2016-01-30 06:04:09 +0100

    Automatic sequence of spreading evil instead of step-by-step clicking.

--- Scripts/ReleaseEvil.cs
+++ Scripts/ReleaseEvil.cs
@@ -3,7 +3,29 @@ using System.Collections;
 
 public class ReleaseEvil : MonoBehaviour {
 
+	// Stop the evil after this many steps.
+	public int maxSteps = 10;
+	// Time between the steps, when playing automatically.
+	public float timeBetweenSteps = 1f;
+	// Is the evil on the move?
+	protected bool progress = false;
+
 	void OnMouseDown() {
+		if (!progress) {
+			StartCoroutine (StepSequence());
+		}
+	}
+
+	protected IEnumerator StepSequence () {
+		progress = true;
+		for (int i = 0; i < maxSteps; i++) {
+			MoveOneStep ();
+			yield return new WaitForSeconds (timeBetweenSteps);
+		}
+		progress = false;
+	}
+
+	protected void MoveOneStep() {
 		GameObject[] tileObjects = GameObject.FindGameObjectsWithTag("Tile");
 
 		foreach (GameObject tileObject in tileObjects) {
