7c59182f6a45e42fc74863caa8cf67619fea9162
nevaermorr <pi.wiacek@gmail.com>
2016-01-30 03:13:57 +0100

    First steps toward changing state of tiles.

--- Scripts/Board.cs
+++ Scripts/Board.cs
@@ -6,6 +6,7 @@ public class Board : MonoBehaviour {
 	public int sizeX = 20;
 	public int sizeY = 20;
 	public float tileToUnitRatio = 5;
+	public float tilePixelSize = 100;
 	public GameObject tilePrefab;
 	// Use this for initialization
 	void Start () {
@@ -24,25 +25,28 @@ public class Board : MonoBehaviour {
 	}
 
 	void CreateBackground() {
-		Vector3 scale = new Vector3 (tileToUnitRatio * sizeX, tileToUnitRatio * sizeY, 1);
+//		Vector3 scale = new Vector3 (tileToUnitRatio * sizeX, tileToUnitRatio * sizeY, 1);
+		Vector3 scale = new Vector3 (sizeX, sizeY, 1);
 		transform.localScale = scale;
 	}
 
 	void GenerateTiles() {
 		for (int i = 0; i < sizeX; i++) {
 			for (int j = 0; j < sizeY; j++) {
-				Vector3 startingPoint = new Vector3 (-sizeX / 2f, -sizeY / 2f, 0) * (tileToUnitRatio/10f);
-//				Vector3 startingPoint =Vector3.zero;
+				Vector3 startingPoint = new Vector3 ((tilePixelSize/100 - sizeX) / 2f, (tilePixelSize/100 - sizeY) / 2f, 0);
+//				Vector3 startingPoint = Vector3.zero;
+
 				GameObject tile = Instantiate (
 					tilePrefab, 
-					startingPoint + (new Vector3 (i, j, 0) * (tileToUnitRatio/10f)),
+					startingPoint + (new Vector3 (i, j, 0)),
 					Quaternion.identity
 				) as GameObject;
-
-//				GameObject Tile = Instantiate (TilePrefab) as GameObject;
+				tile.name = "Tile [" + i + ":" + j + "]";
 				tile.transform.parent = gameObject.transform;
-//				tile.transform.localScale = Vector3.one;
-//				tile.transform.localPosition = new Vector3 (i, j, 0) * (TileToUnitRatio / 10f);
+
+				if (i > 7 && i < 11 && j > 8 && j < 11) {
+					tile.GetComponent<Tile> ().state = Tile.State.evil;
+				}
 			}
 		}
 	}
