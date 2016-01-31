71a96842f928533771834b1553704e4a3b883707
jklimek <jklimek@fidolabs.com>
2016-01-31 12:01:18 +0100

    Added functional menu

---
+++ MenuHover.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHover : MonoBehaviour {
+
+	public SpriteRenderer rend;
+	public int level;
+	public Board board;
+	public GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+		rend = gameObject.GetComponent<SpriteRenderer> ();
+		rend.enabled = false;
+		board = GameObject.FindWithTag ("Board").GetComponent<Board>();
+		menu = GameObject.FindWithTag ("Menu");
+
+	}
+
+	void OnMouseEnter() {
+		rend.enabled = true;
+	}
+
+	void OnMouseExit() {
+		rend.enabled = false;
+	}
+
+	void OnMouseDown() {
+		board.cultistsNumber = level;
+		board.Regenerate ();
+		Vector3 pos = menu.transform.position;
+		pos.z = 500;
+		menu.transform.position = pos;
+	}
+
+}
+
---
+++ MenuOpener.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+
+public var menu: GameObject;
+
+function Start () {
+	menu = GameObject.FindWithTag("Menu");
+}
+
+function OnMouseDown() {
+	menu.transform.position.z = 1;
+}
