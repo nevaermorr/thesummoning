71c2c3323bb1909beea5645d53a558f9b01e8df3
nevaermorr <pi.wiacek@gmail.com>
2016-01-31 08:11:42 +0100

    Game stops when breeding cycle is stuck.

--- Scripts/EvilReleaser.cs
+++ Scripts/EvilReleaser.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
 public class EvilReleaser : MonoBehaviour {
@@ -16,34 +17,71 @@ public class EvilReleaser : MonoBehaviour {
 
 	void OnMouseDown() {
 		if (!progress) {
-			StartCoroutine (StepSequence());
+			StartCoroutine (BreedSequence());
 		}
 	}
 
-	protected IEnumerator StepSequence () {
+	protected IEnumerator BreedSequence () {
 		progress = true;
+		Tile[] tiles = GetTiles ();
+		SequenceTracker sequenceTracker = new SequenceTracker ();
+
 		for (int i = 0; i < maxSteps; i++) {
-			MoveOneStep ();
+			MoveOneStep (tiles);
+
+			if (isEndOfGame (tiles)) {
+				break;
+			} else if(sequenceTracker.isPrevSequence(tiles)) {
+				break;
+			}
+
 			yield return new WaitForSeconds (timeBetweenSteps);
 		}
 		progress = false;
 	}
 
-	protected void MoveOneStep() {
+	protected void MoveOneStep(Tile[] tiles) {
+
+		foreach (Tile tile in tiles) {
+			tile.AssertNextState ();
+		}
+		foreach (Tile tile in tiles) {
+			tile.ChangeToNextState ();
+		}
+	}
+
+	protected Tile[] GetTiles() {
 		GameObject[] tileObjects = GameObject.FindGameObjectsWithTag("Tile");
 
+		Tile[] tiles = new Tile[0];
 		foreach (GameObject tileObject in tileObjects) {
 			Tile tile = tileObject.GetComponent<Tile> ();
 			if (tile) {
-				tileObject.GetComponent<Tile> ().AssertNextState ();
+				ArrayUtility.Add (ref tiles, tileObject.GetComponent<Tile> ());
 			}
 		}
-		foreach (GameObject tileObject in tileObjects) {
-			Tile tile = tileObject.GetComponent<Tile> ();
-			if (tile) {
-				tileObject.GetComponent<Tile> ().ChangeToNextState ();
+		return tiles;
+	}
+
+	protected bool isEndOfGame(Tile[] tiles) {
+		int cultistCount = 0;
+		int evilCount = 0;
+
+		foreach (Tile tile in tiles) {
+			if (tile.state == Tile.State.cultist) {
+				cultistCount++;
+			} else if (tile.state == Tile.State.evil) {
+				evilCount++;
 			}
 		}
+
+		if (cultistCount == 0
+			|| evilCount == 0
+		) {
+			return true;
+		}
+
+		return false;
 	}
 
 	protected void AdjustButtonAlpha() {
---
+++ Scripts/SequenceTracker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SequenceTracker {
+
+//	protected string[] sequences;
+	protected string prevSequence;
+
+	protected string SequenceToString(Tile[] tiles) {
+		SortedDictionary<string, string> tileDictionary = new SortedDictionary<string, string>();
+
+		foreach (Tile tile in tiles) {
+			string index = tile.transform.position.x
+				+ "-"
+				+ tile.transform.position.y;
+			string value = tile.state.ToString ();
+			tileDictionary.Add (index, value);
+		}
+
+		string sequence = "";
+		foreach (KeyValuePair<string, string> pair in tileDictionary) {
+			sequence += pair.Key + "-" + pair.Value + " ";
+		}
+		return sequence;
+	}
+
+	public bool isPrevSequence(Tile[] tiles) {
+		string sequence = SequenceToString (tiles);
+		bool answer = (sequence == prevSequence);
+		prevSequence = sequence;
+		return answer;
+	}
+}
