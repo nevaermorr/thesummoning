435a15edd4b28406909c3af71a480d4abbbec6e4
nevaermorr <pi.wiacek@gmail.com>
2016-01-31 11:03:40 +0100

    Placeholders for win and loss.

---
+++ EndScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScreen : MonoBehaviour {
+	public void Win() {
+		Debug.Log ("you won");	
+	}
+	public void Loss() {
+
+		Debug.Log ("you lost");
+	}
+}
